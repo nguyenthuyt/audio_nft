7281b9cc600cc141646f9db04fac8528cbc0db34
angela <angela.richter1@gmail.com>
2022-05-28 23:28:01 -0500

    sounds and background

--- login_2.py
+++ login_2.py
@@ -3,11 +3,24 @@ import streamlit_authenticator as stauth
 import plotly.graph_objects as go
 import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+import io
+from PIL import Image
+import base64
+#background picture
 
+bg_ext = 'jpg'
 
-
-#page title
-st.title('Just Audio NfT')
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{bg_ext};base64,{base64.b64encode(open("Images/image_#1_soundNFT_bg.jpg", "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 #Accounts, username, password functionality
 names = ['Angela Richter','Jas Pinglia', 'Thuy Nguyen', 'Neil Mendelow']
@@ -27,7 +40,7 @@ name, authentication_status, username = authenticator.login('Login','main')
 if authentication_status:
     authenticator.logout('Logout', 'main')
     st.write('Welcome *%s*' % (name))
-    st.title('Your JANT Account!')
+    st.header('Your JANT Account!')
 
 elif authentication_status == False:
     st.error('Username/password is incorrect')
@@ -37,7 +50,17 @@ elif authentication_status == None:
 
 if authentication_status == True:
     
-    interactive = st.container()
+    audio_file1 = open('./Sounds/covidcough.wav', 'rb') #enter the filename with filepath
+
+    audio_bytes1 = audio_file1.read() #reading the file
+
+    st.audio(audio_bytes1, format='audio/wav') #displaying the audio
+    category = st.multiselect(
+     'Select Categories',
+     ['Green', 'Yellow', 'Red', 'Blue'])
+
+    if category is not None:
+        st.write('You selected:', category)
     #NFT Dataframe
     data = {'Name': ['NFT1', 'NFT2', 'NFT3', 'NFT4'],
         'Category': ['test', 'cat', 'dog', 'baby'],
@@ -68,6 +91,8 @@ if authentication_status == True:
     selected = grid_response['selected_rows'] 
     dfs = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
     
+
+
 
 
 
